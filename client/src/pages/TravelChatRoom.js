581ae097978ac192328772f9317e8b97aa22f3ab
Swayyyy <spradhan22@iitk.ac.in>
2024-03-17 21:20:11 +0530

    chatrooms,preloader

--- client/src/pages/TravelChatRoom.js
+++ client/src/pages/TravelChatRoom.js
@@ -2,19 +2,13 @@ import { useEffect, useRef, useState } from 'react';
 import io from 'socket.io-client';
 import { useNavigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
-
+import dp4 from '../Assets/dp4.png'
 export default function TravelChatRoom() {
     const navigate = useNavigate();
     const socket = useRef(null);
     const location = useLocation();
 
-    let trip_name, destination;
-    try{
-         ({ trip_name, destination } = location.state);
-    } catch{
-        navigate('/home');
-    }
-
+    const { trip_name, destination } = location.state;
     const [info, setInfo] = useState({});
     const [message, setMessage] = useState('');
     const [username, setUsername] = useState('Anonymous'); 
@@ -128,30 +122,71 @@ export default function TravelChatRoom() {
     } else {
         return (
             <div>
-                <h1 className='text-center'>Hi! {username}, Let's Chat</h1>
-                <h2 className='text-center'>{trip_name}</h2>
-                <h2 className='text-center'>Destination : {destination}</h2>
+                 <div>
+                 <div className='bg-white h-[50em] box-border font-serif'>
+                    <div className='flex justify-center items-center wpbg min-h-[100%]'>
+                        <div className='relative w-[87.25em] max-w-[100%] h-[calc(100vh-2.53em)] bg-white sshdw overflow-x-auto'>
+                <div className='relative flex w-full h-[3.75em]
+      bg-[#ededed]'>
+        <div className='w-[5em]'>
+         <img src={dp4} className='ssrch h-[3.2em] w-[3.8em] pl-[0.93em] pt-[0.5em]'/>
+        </div>
+        <div className='w-[18em] font-sans'>
+        <div className='h-[1.5em] pt-1 text-xl'> {trip_name}</div>
+        <div className='h-[1.25em] pt-[0.04em] text-sm'>20 members</div>
+        </div>
+      </div>
+      <div className='relative w-full h-[calc(100%-7.5em)] pt-[1.25em] overflow-x-hidden overflow-y-auto'>
                 <ul>
                     {messages.map((message, index) => {
                         if (message.message.startsWith(trip_name + destination)) {
                             const newMessage = message.message.substring((trip_name + destination).length);
-                            return (
-                                <li key={index} className="user-message">
-                                    <span className="user">{message.username}:</span>
-                                    <span className="message">{newMessage}</span>
+                            if(username == message.username) return (<>
+                                <li key={index} className="">
+                                <div className='relative w-full pl-[66.5em] ml-1 mt-[0.3em] mb-[0.3em]'>
+                                <div className='flex -mb-2'>
+                                <div className='rounded-[50%] w-2 h-2 mt-2 mr-2 bg-[rgb(101,209,119)]'></div>
+                                <p className='mb-[-.13em] mr-2'>{message.username}</p>
+                                <p className='mb-[-0.9em] text-gray-500'>12:15</p>
+                                </div>
+                                    <span class="top-bot"></span>
+                                    <span class="top-bot top-bot-border"></span>
+                                    <p  className='justify relative right-0 mb-[0.93em] text-justify w-[17em] p-[0.75em] bg-[rgb(101,209,119)] rounded-lg overflow-hidden break-words  h-fit'>{newMessage}</p>
+                                    </div>
                                 </li>
+                               </>
+                            );
+                            else return (<>
+                                <li key={index} className="">
+                                <div className='relative w-full mt-[0.3em] -ml-3 mb-[0.3em]'>
+                                <div className='flex -mb-2'>
+                                <div className='rounded-[50%] w-2 h-2 mt-2 mr-2 bg-[#5dbff6]'></div>
+                                <p className='mb-[-.13em] mr-2'>{message.username}</p>
+                                <p className='mb-[-0.9em] text-gray-500'>12:15</p>
+                                </div>
+                                    <span class="top-bot1"></span>
+                                    <span class="top-bot1 top-bot-border"></span>
+                                    <p  className='justify relative right-0 mb-[0.93em] text-justify w-[17em] p-[0.75em] bg-[#5dbff6] rounded-lg overflow-hidden break-words h-fit'>{newMessage}</p>
+                                    </div>
+                                </li>
+                               </>     
                             );
                         } else {
                             return null;
                         }
                     })}
                 </ul>
-                <input value={message} onChange={e => setMessage(e.target.value)} onKeyDown={handleKeyPress} placeholder="message" />
-                <button className="btn btn-primary mt-3" onClick={handleButtonSend}>Send</button>
-
+      </div>
+             <div className='relative w-full h-[3.75em] sinp justify-between flex p-[0.31em] bg-[#f0f0f0] items-center'>
+            <input value={message} onChange={e => setMessage(e.target.value)} onKeyDown={handleKeyPress} className='relative w-[90%]  rounded-[1.8em] text-[1em] pl-4 h-[2.56em]' placeholder='Type your message'/>
+             </div> 
+                </div>
+                </div>
+                </div>
                 <div className="d-flex justify-content-center">
                     <button className="btn btn-primary mt-3" onClick={handleBackPage}>Go Back</button>
                 </div>
+            </div>
             </div>
         );
     }
